83a09c07d3008ad4ffd4292e53a65bec26c25330
gustavosousa07 <gustavorodrigues11cr@gmail.com>
2024-11-11 16:11:20 -0300

    mudei a logo e as cores

---
+++ script.js
@@ -0,0 +1,16 @@
+function registrarPonto() {
+    const messageElement = document.getElementById("message");
+    const currentDate = new Date();
+    messageElement.textContent = `Ponto registrado com sucesso em ${currentDate.toLocaleString()}`;
+  }
+  
+  function registrarHoraExtra() {
+    const messageElement = document.getElementById("message");
+    messageElement.textContent = "Hora extra registrada com sucesso!";
+  }
+  
+  function consultarSalario() {
+    const messageElement = document.getElementById("message");
+    messageElement.textContent = "Seu salário atual é de R$ 3.000,00";
+  }
+  
